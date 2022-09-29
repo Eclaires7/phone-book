ed8da18d497427669eeddb90244bd2e067ae2955
Felicia <feliciakasinda.kun@gmail.com>
2022-09-29 23:24:31 +0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
 import './App.css';
+
+let name = 'aa';
+console.log('🍕 ~ %c Console ', 'background:cadetblue; color:white;', ' ~ name', name)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={css`
+    background-color: hotpink;
+  `}>
+      Hahahaaa
     </div>
   );
 }
